10a193a076726dcffb2fa7298525cbc0cd723e5f
sukhrob22 <ergashevsuxrob986@gmail.com>
2023-09-19 17:16:08 +0500

    add loader and helf timer

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 window.addEventListener('DOMContentLoaded', () => {
     const tabsParent = document.querySelector('.tabheader__items'),
         tabs = document.querySelectorAll('.tabheader__item'),
-        tabsContent = document.querySelectorAll('.tabcontent');
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        loader = document.querySelector('.loader');
 
+    console.log(loader);
+
+    // Loader
+    setTimeout(() => {
+        loader.style.opacity = '0';
+        setTimeout(() => {
+            loader.style.display = 'none';
+        }, 500);
+    }, 2000);
+
+    // Tabs
     function hideTabContent() {
         tabsContent.forEach((item) => {
             item.classList.add('hide');
@@ -34,4 +46,18 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
+
+    // Timer
+
+    const deadline = '2023-09-22';
+
+    function getTimeRemaining(endtime) {
+        const timer = Date.parse(deadline) - Date.parse(new Date()),
+            days = Math.floor(timer / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((timer / (1000 * 60)) % 60),
+            minutes = Math.floor(((timer / 1000) * 60) % 60),
+            seconds = Math.floor((timer / 1000) % 60);
+
+        return { timer, days, hours, minutes, seconds };
+    }
 });
