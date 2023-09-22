c4e353354622a9e35c28ea891a598a9ce52b3b48
sukhrob22 <ergashevsuxrob986@gmail.com>
2023-09-22 09:27:27 +0500

    to add class to project

--- js/script.js
+++ js/script.js
@@ -49,7 +49,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Timer
 
-    const deadline = '2023-09-22';
+    const deadline = '2023-09-23';
 
     function getTimeRemaining(endtime) {
         let days, hours, minutes, seconds;
@@ -142,7 +142,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    const modalTimerId = setTimeout(openModal, 3000);
+    // const modalTimerId = setTimeout(openModal, 3000);
 
     function showModalBYScroll() {
         if (
@@ -155,4 +155,84 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     window.addEventListener('scroll', showModalBYScroll);
+
+    // Class
+
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector, ...clasess) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.clasess = clasess;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 1000;
+            this.changeToUzs();
+        }
+
+        changeToUzs() {
+            this.price = this.price * this.transfer;
+        }
+
+        render() {
+            const element = document.createElement('div');
+
+            if (this.clasess.length == 0) {
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            } else {
+            }
+
+            this.clasess.forEach((classname) =>
+                element.classList.add(classname)
+            );
+
+            element.innerHTML = `
+        
+                <img src=${this.src} alt=${this.alt}/>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">
+            ${this.descr}
+          </div>
+          <div class="menu__item-divider"></div>
+          <div class="menu__item-price">
+            <div class="menu__item-cost">Price:</div>
+            <div class="menu__item-total"><span>${this.price}</span> month</div>
+          </div>
+        
+            `;
+
+            this.parent.append(element);
+        }
+    }
+
+    new MenuCard(
+        'img/tabs/1.png',
+        'usual',
+        'Plan "Usual"',
+        ' Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit nesciunt facere, sequi exercitationem  praesentium ab cupiditate beatae debitis perspiciatis itaque quaerat id modi corporis delectus ratione nobis  harum voluptatum in.',
+        10,
+        '.menu .container'
+    ).render();
+
+    new MenuCard(
+        'img/tabs/2.jpg',
+        'plan',
+        'Plan "Permium"',
+        ' Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit nesciunt facere, sequi exercitationem  praesentium ab cupiditate beatae debitis perspiciatis itaque quaerat id modi corporis delectus ratione nobis  harum voluptatum in.',
+        20,
+        '.menu .container',
+        'menu__item'
+    ).render();
+
+    new MenuCard(
+        'img/tabs/3.jpg',
+        'vip',
+        'Plan Vip',
+        ' Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit nesciunt facere, sequi exercitationem  praesentium ab cupiditate beatae debitis perspiciatis itaque quaerat id modi corporis delectus ratione nobis  harum voluptatum in.',
+        30,
+        '.menu .container',
+        'menu__item'
+    ).render();
 });
