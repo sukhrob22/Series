e0e1e5134425e01b79404a9f01413584ae7d6ea7
sukhrob22 <ergashevsuxrob986@gmail.com>
2023-09-27 09:32:43 +0500

    some way to build slider

--- js/script.js
+++ js/script.js
@@ -374,4 +374,53 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }, 4000);
     }
+
+    //slider
+
+    const slides = document.querySelectorAll('.offer__slide'),
+        next = document.querySelector('.offer__slider-next'),
+        prev = document.querySelector('.offer__slider-prev'),
+        total = document.querySelector('#total'),
+        current = document.querySelector('#current');
+
+    let slideIndex = 1;
+
+    showSlides(slideIndex);
+
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+    } else {
+        total.textContent = slides.length;
+    }
+
+    function showSlides(idx) {
+        if (idx > slides.length) {
+            slideIndex = 1;
+        }
+
+        if (idx < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach((item) => (item.style.display = ' none'));
+        slides[slideIndex - 1].style.display = 'block';
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+    }
+
+    function plusSlides(idx) {
+        showSlides((slideIndex += idx));
+    }
+
+    next.addEventListener('click', () => {
+        plusSlides(1);
+    });
+
+    prev.addEventListener('click', () => {
+        plusSlides(-1);
+    });
 });
